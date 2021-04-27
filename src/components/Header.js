b46e78a53b5a21b27418e82439d994df6911577e
jiexilim <jiexilim.ljx@gmail.com>
2021-04-27 16:47:50 +0800

    Create Home and Login components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home.js";
+import Login from "./Login.js";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Challenge Tracker</h1>
-        </header>
-    )
-}
+	return (
+		<header>
+			<h1>Challenge Tracker</h1>
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+						</ul>
+					</nav>
+					<Switch>
+						<Route exact path="/" component={Home} />
+                        <Route exact path="/login"component={Login} />
+					</Switch>
+				</div>
+			</Router>
+			;
+		</header>
+	);
+};
 
-export default Header
+export default Header;
