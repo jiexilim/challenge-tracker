e7687f2f6abf871c87e58ae54a14e0e1c2df653c
jiexilim <jiexilim.ljx@gmail.com>
2021-06-17 10:49:54 +0800

    Update styles

--- src/components/admin/Home.js
+++ src/components/admin/Home.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Dashboard from "./Dashboard"
 
 const Home = () => {
     const userAccess = localStorage.getItem("userAccess");
 
+    useEffect(() => {
+    }, [])
+
     return (
-            userAccess 
-                ? <Dashboard />
-                : <h1 id="slogan">Aim, plan, execute,<br /> with this challenge tracker.</h1>
-            
+        userAccess
+            ? <Dashboard />
+            : <h1 id="slogan">Aim, plan, execute,<br /> with this challenge tracker.</h1>
+
     )
 }
 
